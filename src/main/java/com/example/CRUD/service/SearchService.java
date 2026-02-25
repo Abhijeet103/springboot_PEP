907dda91a90a0486fb77fd442477380d122dcd2e
Abhijeet <abhijeet@spotter.ai>
2026-02-25 12:58:33 +0530

    search controller

--- src/main/java/com/example/CRUD/service/SearchService.java
+++ src/main/java/com/example/CRUD/service/SearchService.java
@@ -1,8 +1,11 @@
 package com.example.CRUD.service;
 
 
+import com.example.CRUD.dto.GlobalSearchResponseDto;
+import com.example.CRUD.dto.PostResponseDto;
 import com.example.CRUD.dto.UserDto;
 import com.example.CRUD.dto.UserResponseDto;
+import com.example.CRUD.entity.Post;
 import com.example.CRUD.entity.User;
 import com.example.CRUD.entity.UserInfo;
 import com.example.CRUD.repository.PostRepository;
@@ -33,8 +36,20 @@ public class SearchService {
         return userDto;
     }
 
+    private PostResponseDto toDto(Post post){
+        PostResponseDto dto = new PostResponseDto();
+        dto.setUsername(post.getUser().getUsername());
+        dto.setContent(post.getContent());
+        dto.setCreatedAt(post.getCreatedAt());
+        dto.setMediaUrl(post.getMediaUrl());
+        dto.setLikeCount(post.getLikes().size());
+        dto.setCommentsCount(post.getComments().size());
 
-    Page<UserResponseDto> searchUser(String username , int page , int size){
+        return dto;
+    }
+
+
+    public Page<UserResponseDto> searchUser(String username , int page , int size){
         Pageable pageable = PageRequest.of(page, size);
         Page<User> users = userRepository.findByUsernameContainingIgnoreCase(username , pageable) ;
 
@@ -42,5 +57,34 @@ public class SearchService {
     }
 
     // search post
+
+    public Page<PostResponseDto> searchPost(String content , int page , int size){
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Post> posts = postRepository.findByContentContainsIgnoreCase(content, pageable);
+
+        return posts.map(this::toDto);
+
+    }
+
+    public GlobalSearchResponseDto searchGlobalSearch(String content , int page , int size , String type){
+        GlobalSearchResponseDto response = new GlobalSearchResponseDto();
+        boolean isUserSearch = "user".equalsIgnoreCase(type);
+        boolean isPostSearch = "post".equalsIgnoreCase(type);
+
+        if (type == null || type.isBlank()) {
+            isUserSearch = true;
+            isPostSearch = true;
+        }
+
+        if(isUserSearch){
+            response.setUsers(searchUser(content,page,size));
+        }
+
+        if(isPostSearch){
+            response.setPosts(searchPost(content,page,size));
+        }
+
+        return response;
+    }
 
 }
